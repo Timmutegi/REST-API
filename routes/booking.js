8ef4813c8d435e151fe3b8ab482ef395f64558b8
Timothy Mutegi <mutegi.timothy538@gmail.com>
2020-02-04 14:22:56 +0300

    modified get bookings route

--- routes/booking.js
+++ routes/booking.js
@@ -9,7 +9,7 @@ const { bookingValidation } = require('../validation');
 router.get('/', async(req, res) => {
     try {
         // const bookings = await Booking.find();
-        const bookings = await Bookingr.find().populate(User);
+        const bookings = await Booking.find().populate(User);
         // const bookings = await Booking.aggregate([{
         //     $lookup: {
         //         from: user,
