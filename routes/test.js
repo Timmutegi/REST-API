7a512a197542429338e5683a7e8be20fb7ba9461
Timothy Mutegi <mutegi.timothy538@gmail.com>
2020-02-19 20:10:58 +0300

    added validation and status codes

--- routes/test.js
+++ routes/test.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const Test = require('../models/test');
+const { testValidation } = require('../validation')
 
 // SUBMIT
 router.post('/', async(req, res) => {
+    const { error } = testValidation(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
     const test = new Test({
         firstname: req.body.firstname,
         lastname: req.body.lastname,
@@ -13,13 +17,13 @@ router.post('/', async(req, res) => {
         kra: req.body.kra,
         companyName: req.body.companyName,
         companyLocation: req.body.companyLocation,
-        companyRevenue: req.body.compnayRevenue
+        companyRevenue: req.body.companyRevenue
     });
 
     try {
         const savedTest = await test.save();
-        res.send({ savedTest })
+        res.status(200).send({ savedTest, code: 200 })
     } catch (err) {
-        res.json({ message: err });
+        res.status(400).json({ message: err });
     }
 });
