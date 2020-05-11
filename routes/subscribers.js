e4e05fe82880c8d09aa54ef66bc8ab68be520f85
Timothy Mutegi <mutegi.timothy538@gmail.com>
2020-05-11 17:55:15 +0300

    created subscribe get and post routes

---
+++ routes/subscribers.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const router = express.Router();
+const Subscribers = require('../models/subscribers');
+const { subscribeValidation } = require('../validation');
+
+// GET ALL SUBSCRIBERS
+router.get('/', async(req, res) => {
+    try {
+        const subscribers = await Subscribers.find();
+        res.json(subscribers);
+    } catch (err) {
+        res.json({ message: err });
+    }
+});
+
+// SUBMIT SUBSCRIPTION
+router.post('/', async(req, res) => {
+    const { error } = subscribeValidation(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    const subscriber = new Subscribers({
+        email: req.body.email,
+    });
+
+    try {
+        const success = await subscriber.save();
+        res.status(200).send({ code: 200, message: 'successful' })
+
+    } catch {
+        res.json({ message: err });
+    }
+});
+
+module.exports = router;
