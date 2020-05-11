754d94958485588f353aec953a697cb2399b4f66
Timothy Mutegi <mutegi.timothy538@gmail.com>
2020-05-11 17:54:39 +0300

    created subscribe model

---
+++ models/subscribers.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const mailSchema = mongoose.Schema({
+    email: {
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model('Mail', mailSchema);
