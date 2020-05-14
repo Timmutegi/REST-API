43352471106789efe1ec81b7eb44fca59cbdef9a
Timothy Mutegi <mutegi.timothy538@gmail.com>
2020-05-14 09:50:52 +0300

    Created google strategy

---
+++ googleStrategy.js
@@ -0,0 +1,37 @@
+const passport = require('passport');
+const googleStrategy = require('passport-google-oauth20').Strategy;
+const config = require('./config');
+
+function extractProfile(profile) {
+    let imageUrl = '';
+
+    if (profile.photos && profile.photos.length) {
+        imageUrl = profile.photos[0].value;
+    }
+
+    return {
+        id: profile.id,
+        displayName: profile.displayName,
+        image: imageUrl,
+    };
+}
+
+passport.use(new googleStrategy({
+        clientID: config.clientId,
+        clientSecret: config.clientSecret,
+        callbackURL: config.callback,
+        accessType: 'offline',
+        userProfileURL: 'https://www.googleapis.com/oauth2/v3/userinfo',
+    },
+    (accessToken, refreshToken, profile, cb) => {
+        cb(null, extractProfile(profile));
+    }
+));
+
+passport.serializeUser((user, cb) => {
+    cb(null, user);
+});
+
+passport.deserializeUser((obj, cb) => {
+    cb(null, obj);
+});
