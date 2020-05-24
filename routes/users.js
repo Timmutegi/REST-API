bae08516d3ff04f3b7cf2fe7e4f12908d50fe129
Timothy Mutegi <mutegi.timothy538@gmail.com>
2020-05-24 22:52:08 +0300

    updated change password method

--- routes/users.js
+++ routes/users.js
@@ -14,9 +14,9 @@ router.get('/:userID', async(req, res) => {
 });
 
 // CHANGE PASSWORD OF USER
-router.patch('/password', async(req, res) => {
+router.patch('/password/:userID', async(req, res) => {
 
-    const user = await User.findById(req.body.id);
+    const user = await User.findById(req.params.userID);
     if (!user) return res.status(400).send({ code: 400, details: 'User Does Not Exist' });
 
     const validPass = await bcrypt.compare(req.body.password, user.password);
@@ -27,6 +27,7 @@ router.patch('/password', async(req, res) => {
 
     try {
         User.updateOne({ _id: req.body.id }, { $set: { password: newHashedPassword } });
+        res.status(200).send({ code: 200, message: 'Updated Successfully' });
     } catch (err) {
         res.status(400).send({ message: err });
     }
