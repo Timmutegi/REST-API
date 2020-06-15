2cb51ad8a921790b208567ab6543ebb2a72240cc
Timothy Mutegi <mutegi.timothy538@gmail.com>
2020-06-15 20:38:02 +0300

    debugging

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ const swaggerOptions = {
 };
 
 const swaggerDocs = swaggerJsDoc(swaggerOptions);
-app.use('/api-docs', swaggerUi.serve, swaggerui.setup(swaggerDocs));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.use(express.json());
 app.use(cors());
