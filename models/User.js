8d7c48d4e5a892b4de7c0495c2043c4785b4ec7a
Timothy Mutegi <mutegi.timothy538@gmail.com>
2020-06-16 21:28:00 +0300

    documented user model

--- models/User.js
+++ models/User.js
@@ -1,5 +1,34 @@
 const mongoose = require('mongoose');
-
+/**
+ * @swagger
+ *  components:
+ *      schemas;
+ *          User:
+ *              type: object
+ *              required:
+ *                  -firstname
+ *                  -lastname
+ *                  -email
+ *                  -phone
+ *                  -password
+ *              properties:
+ *                  name:
+ *                      type: string
+ *                  lastname:
+ *                      type: string
+ *                  email:
+ *                      type: string
+ *                      format: email
+ *                      description: Email of the user, needs to be unique
+ *                  phone:
+ *                      type: number
+ *                      description: Phone Number of the user, needs to be unique
+ *                  password:
+ *                      type: string
+ *                      description: Must be a minimum of 6 characters
+ *                      
+ * 
+ */
 const UserSchema = mongoose.Schema({
     firstname: {
         type: String,
@@ -22,7 +51,7 @@ const UserSchema = mongoose.Schema({
     },
     password: {
         type: String,
-        require: true,
+        required: true,
         max: 1024,
         min: 6
     },
