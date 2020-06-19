b5959d03f9c84ae61150cb830d3f702c1bcc0f97
Timothy Mutegi <mutegi.timothy538@gmail.com>
2020-06-19 17:54:33 +0300

    debugging

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ const swaggerOptions = {
             url: 'https://obscure-beyond-81246.herokuapp.com/api',
         }
     },
-    apis: ['./routes/*.js']
+    apis: ['./routes/*.js', './models/*.js']
 };
 
 const swaggerDocs = swaggerJsDoc(swaggerOptions);
