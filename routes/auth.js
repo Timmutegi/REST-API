f6223ed4563d9258bc93cd4d038cae3e00c12de2
Timothy Mutegi <mutegi.timothy538@gmail.com>
2020-06-20 20:21:39 +0300

    updated authentication

--- routes/auth.js
+++ routes/auth.js
@@ -131,7 +131,7 @@ router.patch('/reset', async(req, res) => {
 router.get('/google', passport.authenticate('google', { scope: ['profile'] }))
 
 router.get('/google/callback', passport.authenticate('google', { failureRedirect: '/' }), (req, res) => {
-    res.redirect('/reset')
+    res.redirect('/login')
 })
 
 module.exports = router;
