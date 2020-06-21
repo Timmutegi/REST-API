cd86080e0346613fdc47bf38066e781c6751fe03
Timothy Mutegi <mutegi.timothy538@gmail.com>
2020-06-21 18:15:00 +0300

    Added proxy

--- app.js
+++ app.js
@@ -38,6 +38,10 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.use(express.json());
 app.use(cors());
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+});
 app.use(passport.initialize());
 
 // Import Routes
